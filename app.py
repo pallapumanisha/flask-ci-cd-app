ccb700ad175b90a3ff2574564f41b30cb67b02bd
Manisha <pallapumanisha286@gmail.com>
2025-11-14 11:35:56 +0000

    Test deployment update

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
 @app.route('/')
 def hello():
-    return "Hello from Flask CI/CD App! 🚀"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return "Hello from Flask CI/CD App! ✅ Updated!"
 
