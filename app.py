a8d8089221aeaf56ec1386c51dd91257fd139450
pallapumanisha <pallapumanisha286@gmail.com>
2025-11-19 10:59:44 +0000

    Test CI/CD with runner

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD App! 🚀"
+     return "Hello from Flask CI/CD App! updated via CI/CD 🚀"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
