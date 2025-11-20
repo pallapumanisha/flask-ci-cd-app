77b3068118784cadd790140bb8b0c161388ca6ec
pallapumanisha <pallapumanisha286@gmail.com>
2025-11-20 16:07:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "Hello from Flask CI/CD App!u899"
+     return "Hello from Flask CI/CD App!u899 arerere"
 
 
 if __name__ == "__main__":
