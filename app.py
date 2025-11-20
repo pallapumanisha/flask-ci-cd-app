8b74d56e84133050097e70be26a7b92ce8a50d1a
pallapumanisha <pallapumanisha286@gmail.com>
2025-11-20 16:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "Hello from Flask CI/CD App!u899 arerere"
+    try:
+        client = MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=2000)
+        client.admin.command("ping")
+        message = "MongoDB server is successfully installed"
+    except ConnectionFailure:
+        message = "Failed to connect to MongoDB server"
+    finally:
+        try:
+            client.close()
+        except Exception:
+            pass
 
+    return message
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
