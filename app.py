414fdd3b75962aada10061b3f56ed6276ee738e7
pallapumanisha <pallapumanisha286@gmail.com>
2025-12-02 12:35:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb://mongo:27017/")
+db = client["testdb"]
+collection = db["testcollection"]
+
+@app.route("/")
+def home():
+    collection.insert_one({"message": "Hello from Flask and MongoDB!"})
+    return "Flask + MongoDB connection successful!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
