351f559cfadc567a98d106ad612817b494116282
pallapumanisha <pallapumanisha286@gmail.com>
2025-12-04 11:51:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Flask + Mongo+docker+compose+ci/cd! new"
+    return "Hello Flask + Mongo+docker+compose+ci/cd! new oooo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
