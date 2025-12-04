7b95a04d39d1ddde7489dbcd7d3c0639214fbae8
pallapumanisha <pallapumanisha286@gmail.com>
2025-12-04 11:55:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
+# Connect to MongoDB using docker-compose service name "mongo"
+client = MongoClient("mongodb://mongo:27017")
+db = client["testdb"]
+collection = db["messages"]
+
 @app.route('/')
-def hello():
-    return "Hello Flask + Mongo+docker+compose+ci/cd! new oooo"
+def index():
+    # Insert sample data if not exists
+    collection.insert_one({"msg": "Hello from MongoDB!"})
+
+    # Read last inserted
+    data = collection.find().sort("_id", -1).limit(1)[0]
+    return f"Flask + MongoDB Connected Successfully → {data['msg']}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
